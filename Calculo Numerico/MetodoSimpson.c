45878efd76b54cef64462ee80a3d25341d67bbad
GabrielaGSantos <gabi.gsantos@hotmail.com>
2017-03-17 15:55:55 -0400

    Aplicações múltiplas da Regra de 1/3 de Simpson

--- Calculo Numerico/MetodoSimpson.c
+++ Calculo Numerico/MetodoSimpson.c
@@ -11,7 +11,7 @@ void erro(float limiteInferior, float limiteSuperior, int segmentos);
 
 
 int main() {
-	printf("\n\nMetodo de 1/3 de Simpson\n");
+	printf("\n\nAplicacoes Multiplas da Regra de 1/3 de Simpson\n");
 	MetodoSimpson(4, 0.8, 0);
 	return 0;
 }
@@ -27,26 +27,28 @@ void MetodoSimpson(int segmentos, float limiteSuperior, float limiteInferior) {
 		else {
 			if (segmentos % 2 != 0) segmentos = segmentos + 1; // O numero de segmentos deve ser par
 
-			altura = (limiteSuperior - limiteInferior) / segmentos; // A altura √© calculada dividindo a largura pelo numero de segmentos
-			pontoAtual = limiteInferior + altura; // o limite inferior e o limite superior n√£o s√£o somados na soma dos pares e dos √≠mpares
+			altura = (limiteSuperior - limiteInferior) / segmentos; // A altura È calculada dividindo a largura pelo numero de segmentos
+			pontoAtual = limiteInferior + altura; // o limite inferior e o limite superior nao sao somados na soma dos pares e dos Ìmpares
 			somaImpares = 0; //inicializa a soma dos numeros √≠mpares
 			somaPares = 0;  //inicializa a soma dos numeros pares
 
-			for (i = 1; i < segmentos; i++) { //realiza a soma dos f √≠mpares e dos f pares, at√© segmentos-1, pois o f dos extremos nao √© inclu√≠do na soma dos pares e √≠mpares
-				if (i % 2 == 0) {  //verifica se o pontoAtual √© par, para somar os pares
-					somaPares = somaPares + funcao(pontoAtual);   //somat√≥rio dos f pares
+			for (i = 1; i < segmentos; i++) { //realiza a soma dos f Ìmpares e dos f pares, atÈ segmentos-1, pois o f dos extremos nao È incluÌdo na soma dos pares e Ìmmpares
+				
+				if (i % 2 == 0) {  //verifica se o pontoAtual È par, para somar os pares
+					somaPares = somaPares + funcao(pontoAtual);   //somatÛrio dos f pares
 				}
-				else { // soma dos √≠mpares
-					somaImpares = somaImpares + funcao(pontoAtual); //somat√≥rio dos f √≠mpares
+				else { // soma dos Ìmpares
+					somaImpares = somaImpares + funcao(pontoAtual); //somatÛrio dos f Ìmpares
 				}
-				pontoAtual = pontoAtual + altura; //pr√≥ximo ponto
+				pontoAtual = pontoAtual + altura; //prÛximo ponto
 			}
-
-			area(altura, limiteInferior, limiteSuperior, somaPares, somaImpares, segmentos); //chama a fun√ßao para c√°lcular a integral, utilizando a fun√ßao do m√©todo
+			
+			area(altura, limiteInferior, limiteSuperior, somaPares, somaImpares, segmentos); //chama a funÁao para calcular a integral, utilizando o mÈtodo de simpson
 		}
 }
 
-void area(float altura, float limiteInferior, float limiteSuperior, float somaPares, float somaImpares, int segmentos) { //c√°lculo da integral a partir do m√©todo de simpson
+
+void area(float altura, float limiteInferior, float limiteSuperior, float somaPares, float somaImpares, int segmentos) { //calculo da integral a partir do mÈtodo de simpson
 	float resultado;
 
 	resultado = ((altura / 3) * (funcao(limiteInferior) + funcao(limiteSuperior) + (4 * somaImpares) + 2 * somaPares)); //calculo da integral
@@ -54,13 +56,18 @@ void area(float altura, float limiteInferior, float limiteSuperior, float somaPa
 	printf("\nA area da funcao e': %f", resultado); //imprime o resultado
 	erro(limiteInferior, limiteSuperior, segmentos); //imprime o erro
 }
+
+
 float funcao(float x) {
-	return (0.2 + (25 * x) - (200 * pow(x, 2)) + (675 * pow(x, 3)) - (900 * pow(x, 4)) + (400 * pow(x, 5))); //ifun√ß√£o para c√°lcular a integral
+	return (0.2 + (25 * x) - (200 * pow(x, 2)) + (675 * pow(x, 3)) - (900 * pow(x, 4)) + (400 * pow(x, 5))); //funÁao para calcular a integral
 }
 
-void erro(float limiteInferior, float limiteSuperior, int segmentos) { //c√°lculo do erro
+
+
+void erro(float limiteInferior, float limiteSuperior, int segmentos) { //calculo do erro
 	float erroA;
 
-	erroA = (-(((pow((limiteSuperior - limiteInferior), 5)) / (180 * pow(segmentos, 4))) *(-2400))); //obs: -2400 √© o valor m√©dio da derivada 4
+	erroA = (-(((pow((limiteSuperior - limiteInferior), 5)) / (180 * pow(segmentos, 4))) *(-2400))); //obs: -2400 È o valor mÈdia da derivada 4
 	printf("\nEa = %f \n\n\n\n", erroA);
 } 
+
